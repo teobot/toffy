7999a0d17bc86c85e1b8a534fe41f27580ce7e32
teobot <theoclapperton@outlook.com>
2021-04-13 15:01:49 +0100

    Added new tournamentConfiguration file

---
+++ src/components/Tournament/TournamentConfig.js
@@ -0,0 +1,13 @@
+// Tournament enums
+
+export const tournament_types = {
+  LEADERBOARD: "Leaderboard",
+  DOUBLE_ELIMINATION: "Double Elimination",
+  SINGLE_ELIMINATION: "Single Elimination",
+};
+
+export const states = {
+  JOIN: "Joining",
+  PLAY: "Playing",
+  END: "Ended",
+};
