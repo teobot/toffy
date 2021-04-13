381c9e671777ead4478a16f4bdec24967fd0c461
teobot <theoclapperton@outlook.com>
2021-04-13 15:05:08 +0100

    Added new Model for match confirmation

--- src/screens/TournamentScreen.js
+++ src/screens/TournamentScreen.js
@@ -6,17 +6,23 @@ import { useParams } from "react-router";
 import {
   Button,
   Container,
+  Divider,
   Grid,
   Header,
   Icon,
   Image,
+  Label,
   List,
   Segment,
 } from "semantic-ui-react";
 
 import toffy from "../api/toffy";
-import UserPlayingButtonGroup from "../components/Tournament/UserPlayingButtonGroup";
-import AdminTools from "../components/Tournament/AdminTools";
+
+import UserPlayingButtonGroup from "../components/Tournament/TournamentControls/UserPlayingButtonGroup";
+import AdminTools from "../components/Tournament/AdministrationTools/AdminTools";
+import TournamentBracketSegment from "../components/Tournament/TournamentDisplays/SingleElimination/TournamentBracketSegment";
+
+import { tournament_types } from "../components/Tournament/TournamentConfig";
 
 export default function TournamentScreen() {
   let { _id } = useParams();
@@ -26,6 +32,16 @@ export default function TournamentScreen() {
   useEffect(() => {
     getTournamentData();
   }, []);
+
+  const MatchDataDisplayComponent = (type, match_data, isAdmin) => {
+    if (type === tournament_types.SINGLE_ELIMINATION) {
+      return (
+        <TournamentBracketSegment match_data={match_data} isAdmin={isAdmin} />
+      );
+    }
+
+    return null;
+  };
 
   const getTournamentData = async () => {
     // This function gathers the tournament information
@@ -51,6 +67,7 @@ export default function TournamentScreen() {
   } else {
     return (
       <Container fluid>
+        {/* This displays the title, type, creation time, desc and join/leave buttons in a sub component */}
         <Grid columns="equal">
           <Grid.Row>
             <Grid.Column width="12">
@@ -72,6 +89,7 @@ export default function TournamentScreen() {
             </Grid.Column>
           </Grid.Row>
         </Grid>
+
         {/* Administrator tools for the creator */}
         {result.isAdmin ? (
           <AdminTools
@@ -79,32 +97,58 @@ export default function TournamentScreen() {
             getTournamentData={getTournamentData}
           />
         ) : null}
+
+        {/* Display the match data to the user */}
+        {MatchDataDisplayComponent(
+          result.type,
+          result.match_data,
+          result.isAdmin
+        )}
+
+        {/* This displays the players in a grid */}
         <Segment>
           <Header as="h4">{result.players.length} Players:</Header>
-          <List animated divided verticalAlign="middle">
-            {result.players.map((player) => {
-              return (
-                <List.Item>
-                  <Image
-                    avatar
-                    src="https://react.semantic-ui.com/images/avatar/small/christian.jpg"
-                  />
-                  <List.Content>
-                    <List.Header as="h4">
-                      {player.username}{" "}
-                      {result.userId === player._id ? (
-                        <List.Description>
-                          <small>- this is you 👋</small>
-                        </List.Description>
-                      ) : null}
-                    </List.Header>
-                  </List.Content>
-                </List.Item>
-              );
-            })}
-            {result.players.length < 1 ? "No players" : null}
-          </List>
+          <Grid stretched>
+            {[...result.players]
+              .sort(function compare(a, b) {
+                if (a.elo < b.elo) {
+                  return 1;
+                }
+                if (a.elo > b.elo) {
+                  return -1;
+                }
+                return 0;
+              })
+              .map((player) => {
+                return (
+                  <Grid.Row stretched verticalAlign="middle">
+                    <Grid.Column width={1}>
+                      <Image
+                        rounded
+                        src="https://react.semantic-ui.com/images/avatar/small/christian.jpg"
+                        fluid
+                      />
+                    </Grid.Column>
+                    <Grid.Column width={15}>
+                      <Header as="h3">
+                        {player.username}{" "}
+                        <Label basic horizontal>
+                          <Icon name="gem" />
+                          {player.elo}
+                        </Label>
+                        {result.userId === player._id ? (
+                          <small> - you 👋</small>
+                        ) : null}
+                      </Header>
+                    </Grid.Column>
+                  </Grid.Row>
+                );
+              })}
+            {result.players.length < 1 ? <Grid.Row>No players</Grid.Row> : null}
+          </Grid>
         </Segment>
+
+        <Divider section hidden />
       </Container>
     );
   }
