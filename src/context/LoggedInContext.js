58520ad8d493e69657138e61a72d0830921a6ce1
teobot <theoclapperton@outlook.com>
2021-04-19 21:46:21 +0100

    Updated logicFunction, Added new username and user_id variables into context

--- src/context/LoggedInContext.js
+++ src/context/LoggedInContext.js
@@ -19,22 +19,32 @@ export const TOKEN_STORAGE_NAME = "@token";
 export default () => {
   // This handles the global user of the user location information
   const [UserLoggedIn, setUserLoggedIn] = useState(false);
+
+  // useStates variables
   const [token, setToken] = useState(null);
+  const [username, setUsername] = useState(null);
+  const [user_id, setUser_id] = useState(null);
+
   let history = useHistory();
 
   const handleLogout = () => {
     //   This handles the user logout
     toffy.defaults.headers.common["X-Authorization"] = "";
     StorageHelper.deleteStorage("@token");
-    setUserLoggedIn(false);
     setToken(null);
+    setUsername(null);
+    setUser_id(null);
+    setUserLoggedIn(false);
   };
 
-  const handleUserLogin = (userToken) => {
-    //   This handles the user logging in
-    toffy.defaults.headers.common["X-Authorization"] = userToken;
-    StorageHelper.setStorage("@token", userToken);
-    setToken(userToken);
+  const handleUserLogin = (login_response) => {
+    //  This handles the user logging in
+    //  login_response === { token, username, user_id }
+    toffy.defaults.headers.common["X-Authorization"] = login_response.token;
+    StorageHelper.setStorage("@token", login_response.token);
+    setToken(login_response.token);
+    setUsername(login_response.username);
+    setUser_id(login_response.user_id);
     setUserLoggedIn(true);
   };
 
@@ -44,6 +54,8 @@ export default () => {
       handleLogout,
       handleUserLogin,
       token,
+      username,
+      user_id,
       TOKEN_STORAGE_NAME,
     },
   ];
