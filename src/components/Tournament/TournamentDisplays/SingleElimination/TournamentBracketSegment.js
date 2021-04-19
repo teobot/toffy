120b64a085d270cdd57c3cdb8b19be476c4a2b5a
teobot <theoclapperton@outlook.com>
2021-04-19 21:44:08 +0100

    Redesigned entire UI

--- src/components/Tournament/TournamentDisplays/SingleElimination/TournamentBracketSegment.js
+++ src/components/Tournament/TournamentDisplays/SingleElimination/TournamentBracketSegment.js
@@ -1,4 +1,10 @@
-import React, { useRef, useEffect, createContext, useState } from "react";
+import React, {
+  useRef,
+  useEffect,
+  createContext,
+  useState,
+  useReducer,
+} from "react";
 import {
   List,
   Segment,
@@ -11,6 +17,8 @@ import {
   Modal,
   Image,
   Button,
+  Input,
+  Form,
 } from "semantic-ui-react";
 
 import { TOOL_AUTO, UncontrolledReactSVGPanZoom } from "react-svg-pan-zoom";
@@ -18,35 +26,101 @@ import { TOOL_AUTO, UncontrolledReactSVGPanZoom } from "react-svg-pan-zoom";
 import useWindowWidth from "../../../../functions/useWindowWidth";
 
 import Round from "./components/Round";
-import { set } from "js-cookie";
-import { Model } from "react-tournament-bracket";
+
+import toffy from "../../../../api/toffy";
 
 export const TournamentContext = createContext();
 
-// TODO: After testing add this prop `match_data,`
-export default function RobinRoundDisplay({ match_data, isAdmin }) {
+const matchReducer = (state, action) => {
+  switch (action.type) {
+    case "change_player1Selected":
+      return { ...state, player1Selected: true, player2Selected: false };
+    case "change_player2Selected":
+      return { ...state, player2Selected: true, player1Selected: false };
+    case "change_player1Score":
+      return { ...state, player1Score: action.payload };
+    case "change_player2Score":
+      return { ...state, player2Score: action.payload };
+    case "reset":
+      return {
+        player1Selected: false,
+        player2Selected: false,
+        player1Score: 0,
+        player2Score: 0,
+      };
+    default:
+      return state;
+  }
+};
+
+export default function RobinRoundDisplay({
+  match_data,
+  isAdmin,
+  tournament_id,
+  getTournamentData,
+  inProgress,
+}) {
   const Viewer = useRef(null);
   const { windowWidth, windowHeight } = useWindowWidth();
+
+  const [state, dispatch] = useReducer(matchReducer, {
+    player1Selected: false,
+    player2Selected: false,
+    player1Score: 0,
+    player2Score: 0,
+  });
 
   const [open, setOpen] = useState(false);
   const [modelMatch, setModelMatch] = useState(null);
 
-  const UpdateMatchDetails = (match) => {
-    // console.log(match);
-    setModelMatch(match);
+  const changeSelectedMatch = (Match) => {
+    setModelMatch(Match);
     setOpen(true);
   };
 
+  const handleInputChange = (event, data) => {
+    dispatch({ type: `change_${data.name}`, payload: parseInt(data.value) });
+  };
+
+  const UpdateMatchDetails = async () => {
+    try {
+      const r = await toffy.patch(
+        `/tournament/${tournament_id}/match/${modelMatch._id}`,
+        {
+          winner: state.player1Selected
+            ? "player1"
+            : state.player2Selected
+            ? "player2"
+            : null,
+          scores: {
+            player1: state.player1Score,
+            player2: state.player2Score,
+          },
+        }
+      );
+
+      if (r.status === 200) {
+        // Match winner is successful
+        setOpen(false);
+        await getTournamentData();
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   useEffect(() => {
-    Viewer.current.zoom(0, 0, 1);
+    Viewer.current.zoom(150, 150, 0.7);
   }, []);
 
   return (
-    <TournamentContext.Provider value={{ isAdmin: true, UpdateMatchDetails }}>
-      <Segment inverted>
+    <>
+      <TournamentContext.Provider
+        value={{ isAdmin, inProgress, changeSelectedMatch }}
+      >
         <UncontrolledReactSVGPanZoom
-          background="#272a33"
-          SVGBackground="#2b2b2b"
+          background="#24262e"
+          SVGBackground="#24262e"
           tool={TOOL_AUTO}
           ref={Viewer}
           width="100%"
@@ -62,7 +136,6 @@ export default function RobinRoundDisplay({ match_data, isAdmin }) {
           <svg>
             {match_data.map((roundData, roundIndex) => {
               return (
-                // TODO: the tournamentMatchData props needs to be changed to `matchData`
                 <Round
                   roundData={roundData}
                   roundIndex={roundIndex}
@@ -72,48 +145,104 @@ export default function RobinRoundDisplay({ match_data, isAdmin }) {
             })}
           </svg>
         </UncontrolledReactSVGPanZoom>
+      </TournamentContext.Provider>
 
-        <Modal
-          onClose={() => setOpen(false)}
-          onOpen={() => setOpen(true)}
-          open={open}
-        >
-          <Modal.Header>Edit Match Details</Modal.Header>
+      <Modal
+        onClose={() => {
+          dispatch({ type: "reset" });
+          setOpen(false);
+        }}
+        onOpen={() => setOpen(true)}
+        open={open}
+      >
+        <Modal.Header>Edit Match Details</Modal.Header>
 
-          {modelMatch ? (
-            <Modal.Content>
-              <Header textAlign="center" size="huge">
-                Who won?
-              </Header>
+        {modelMatch ? (
+          <Modal.Content>
+            <Header textAlign="center" size="huge">
+              Who won?
+            </Header>
 
-              <Grid>
-                <Grid.Row textAlign="center" columns="equal">
-                  <Grid.Column>
-                    <Button.Group size="huge">
-                      <Button>{modelMatch.player1.player.username}</Button>
-                      <Button.Or />
-                      <Button>{modelMatch.player2.player.username}</Button>
-                    </Button.Group>
-                  </Grid.Column>
-                </Grid.Row>
-              </Grid>
-            </Modal.Content>
-          ) : null}
+            <Grid stretched>
+              <Grid.Row columns="equal" stretched>
+                <Grid.Column>
+                  <Button
+                    positive={state.player1Selected}
+                    onClick={() => {
+                      dispatch({
+                        type: "change_player1Selected",
+                      });
+                    }}
+                  >
+                    {modelMatch.player1.player.username}
+                  </Button>
+                </Grid.Column>
+                <Grid.Column width={2}></Grid.Column>
+                <Grid.Column>
+                  <Button
+                    positive={state.player2Selected}
+                    onClick={() => {
+                      dispatch({
+                        type: "change_player2Selected",
+                      });
+                    }}
+                  >
+                    {modelMatch.player2.player.username}
+                  </Button>
+                </Grid.Column>
+              </Grid.Row>
 
-          <Modal.Actions>
-            <Button negative onClick={() => setOpen(false)}>
-              Close
-            </Button>
-            <Button
-              content="Save Changes"
-              labelPosition="right"
-              icon="checkmark"
-              onClick={() => setOpen(false)}
-              positive
-            />
-          </Modal.Actions>
-        </Modal>
-      </Segment>
-    </TournamentContext.Provider>
+              <Divider />
+              <Grid.Row columns="equal" stretched>
+                <Grid.Column>
+                  <Input
+                    fluid
+                    label="Player 1 Score"
+                    type="number"
+                    name="player1Score"
+                    value={state.player1Score}
+                    onChange={handleInputChange}
+                  />
+                </Grid.Column>
+                <Grid.Column width={2}></Grid.Column>
+                <Grid.Column>
+                  <Input
+                    fluid
+                    label="Player 2 Score"
+                    type="number"
+                    min={0}
+                    max={9999}
+                    name="player2Score"
+                    value={state.player2Score}
+                    onChange={handleInputChange}
+                    labelPosition="right"
+                  />
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Modal.Content>
+        ) : null}
+
+        <Modal.Actions>
+          <Button
+            negative
+            onClick={() => {
+              dispatch({ type: "reset" });
+              setOpen(false);
+            }}
+          >
+            Close
+          </Button>
+          <Button
+            disabled={!state.player2Selected && !state.player1Selected}
+            content="Save Changes"
+            labelPosition="right"
+            icon="checkmark"
+            onClick={UpdateMatchDetails}
+            positive
+          />
+        </Modal.Actions>
+      </Modal>
+    </>
   );
 }
