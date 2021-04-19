eca75e639f5891db726edab3bab6d36a34149780
teobot <theoclapperton@outlook.com>
2021-04-19 21:47:07 +0100

    Added new displayDate and displayState functions

--- src/functions/calculateCreated.js
+++ src/functions/calculateCreated.js
@@ -1,9 +1,28 @@
+import { states } from "../components/Tournament/TournamentConfig";
+
 const dayjs = require("dayjs");
 const relativeTime = require("dayjs/plugin/relativeTime");
 
 const calculateCreated = (date) => {
   dayjs.extend(relativeTime);
   return dayjs(date).fromNow();
+};
+
+export const displayDate = (date) => {
+  dayjs.extend(relativeTime);
+  return dayjs(date).format(`MMMM DD, YYYY [at] hh:mm A`);
+};
+
+export const displayState = (state) => {
+  if (state === states.JOIN) {
+    return "Joinable";
+  }
+  if (state === states.PLAY) {
+    return "In Progress";
+  }
+  if (state === states.END) {
+    return "Ended";
+  }
 };
 
 export default calculateCreated;
