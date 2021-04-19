17c2e0df56a5a6e547a46ea6abf7bf976bc03806
teobot <theoclapperton@outlook.com>
2021-04-19 21:44:45 +0100

    Added match_id text, Used context for user_id

--- src/components/Tournament/TournamentDisplays/SingleElimination/components/Match.js
+++ src/components/Tournament/TournamentDisplays/SingleElimination/components/Match.js
@@ -7,9 +7,18 @@ import Edit from "../../../../img/edit";
 
 export default function Match({ ThisX, ThisY, match, width, height }) {
   const polylineStyle = { fill: "none", stroke: "black", strokeWidth: 1 };
-  const { isAdmin, UpdateMatchDetails } = useContext(TournamentContext);
+  const { isAdmin, changeSelectedMatch, inProgress } = useContext(
+    TournamentContext
+  );
 
-  const isEditable = !match.winner && isAdmin && match.player1.player && match.player2.player;
+  const isEditable =
+    !match.winner &&
+    isAdmin &&
+    match.player1.player &&
+    match.player2.player &&
+    inProgress;
+
+  const displayMatchId = false;
 
   return (
     <g>
@@ -53,7 +62,7 @@ export default function Match({ ThisX, ThisY, match, width, height }) {
             pointer-events="visible"
             style={{ fill: "none" }}
             onClick={() => {
-              UpdateMatchDetails(match);
+              changeSelectedMatch(match);
             }}
             height={34}
             width={35}
@@ -68,6 +77,30 @@ export default function Match({ ThisX, ThisY, match, width, height }) {
             y={ThisY + 15}
           />
         </g>
+      ) : null}
+
+      {/* Match Id text */}
+      {displayMatchId ? (
+        <>
+          <circle
+            cx={ThisX - 4}
+            cy={ThisY - 4}
+            r={14}
+            style={{ fill: "lightgreen" }}
+          />
+          <text x={ThisX - 8} y={ThisY}>
+            {match.match_id}
+          </text>
+          <circle
+            cx={ThisX + width}
+            cy={ThisY - 4}
+            r={14}
+            style={{ fill: "pink" }}
+          />
+          <text x={ThisX + width - 4} y={ThisY}>
+            {match.next_match_id}
+          </text>
+        </>
       ) : null}
     </g>
   );
