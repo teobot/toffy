129ed8e77c4ae34a9d036a38a4805fccc35c2d44
teobot <theoclapperton@outlook.com>
2021-04-20 20:49:06 +0100

    Added authorized profile images

--- src/components/Tournament/TournamentConfig.js
+++ src/components/Tournament/TournamentConfig.js
@@ -11,3 +11,20 @@ export const states = {
   PLAY: "Playing",
   END: "Ended",
 };
+
+// Allowed User Profile images
+export const ALLOWED_PROFILE_IMAGES = [
+  "https://raw.githubusercontent.com/teobot/toffy-bucket/main/profle-image-001.jpg",
+  "https://raw.githubusercontent.com/teobot/toffy-bucket/main/profle-image-003.jpg",
+  "https://raw.githubusercontent.com/teobot/toffy-bucket/main/profle-image-002.jpg",
+  "https://raw.githubusercontent.com/teobot/toffy-bucket/main/profle-image-004.jpg",
+  "https://raw.githubusercontent.com/teobot/toffy-bucket/main/profle-image-005.jpg",
+  "https://raw.githubusercontent.com/teobot/toffy-bucket/main/profle-image-006.jpg",
+  "https://raw.githubusercontent.com/teobot/toffy-bucket/main/profle-image-007.jpg",
+  "https://raw.githubusercontent.com/teobot/toffy-bucket/main/profle-image-008.jpg",
+  "https://raw.githubusercontent.com/teobot/toffy-bucket/main/profle-image-009.jpg",
+  "https://raw.githubusercontent.com/teobot/toffy-bucket/main/profle-image-010.jpg",
+  "https://raw.githubusercontent.com/teobot/toffy-bucket/main/profle-image-011.jpg",
+  "https://raw.githubusercontent.com/teobot/toffy-bucket/main/profle-image-012.jpg",
+  "https://raw.githubusercontent.com/teobot/toffy-bucket/main/profle-image-013.jpg",
+];
