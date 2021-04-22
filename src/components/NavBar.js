8dea973d04510d40135e7c846757c5ad5a62d705
teobot <theoclapperton@outlook.com>
2021-04-22 16:50:06 +0100

    Updated UI/UIX

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,67 +14,86 @@ import {
 
 import { LoggedInContext } from "../context/LoggedInContext";
 
-import ImageClear from "../img/logoClear.png";
-
 export default function NavBar({ routes }) {
   let history = useHistory();
   const { UserLoggedIn, handleLogout, token, username, user_id } = useContext(
     LoggedInContext
   );
   return (
-    <div>
+    <div id="navbar">
       <Segment inverted clearing padded style={{ borderRadius: 0 }}>
         <Menu inverted>
           <Menu.Item
             onClick={() => {
-              if (UserLoggedIn) {
-                history.push("/Home");
-              } else {
-                history.push("/landing");
-              }
+              history.push(UserLoggedIn ? "/home" : "/");
             }}
-            link
-            name="Home"
-          />
+          >
+            <img
+              alt="company logo"
+              src="https://react.semantic-ui.com/logo.png"
+            />
+          </Menu.Item>
 
-          {UserLoggedIn ? (
+          {false ? (
             <>
               <Menu.Item
                 onClick={() => {
                   history.push("/create/tournament");
                 }}
                 link
-                name="Create"
+                name="Tournament Creation"
               />
             </>
-          ) : null}
+          ) : (
+            <></>
+          )}
 
           {UserLoggedIn ? (
-            <>
-              <Dropdown className="right link item" placeholder={username}>
-                <Dropdown.Menu>
-                  <Dropdown.Item
-                    onClick={() => {
-                      history.push(`/u/${user_id}`);
-                    }}
-                    text="Profile"
-                  />
-                  <Dropdown.Divider />
-                  <Dropdown.Item
-                    onClick={() => {
-                      if (UserLoggedIn) {
-                        handleLogout();
-                        history.push("/landing");
-                      } else {
-                        history.push("/login");
-                      }
-                    }}
-                    text="Logout"
-                  />
-                </Dropdown.Menu>
-              </Dropdown>
-            </>
-          ) : null}
+            <Dropdown item text={username} className="right">
+              <Dropdown.Menu>
+                <Dropdown.Item
+                  onClick={() => {
+                    history.push(`/u/${user_id}`);
+                  }}
+                  text="Profile"
+                />
+                <Dropdown.Divider />
+                <Dropdown.Item
+                  onClick={() => {
+                    if (UserLoggedIn) {
+                      handleLogout();
+                      history.push("/");
+                    } else {
+                      history.push("/login");
+                    }
+                  }}
+                  text="Logout"
+                />
+              </Dropdown.Menu>
+            </Dropdown>
+          ) : (
+            <Menu.Menu position="right">
+              <Menu.Item
+                onClick={() => {
+                  history.push("/login");
+                }}
+                link
+                name="Login"
+              />
+              <Menu.Item>
+                <Button
+                  onClick={() => {
+                    history.push("/create/account");
+                  }}
+                  color="orange"
+                  compact
+                  size="tiny"
+                >
+                  SIGN UP
+                </Button>
+              </Menu.Item>
+            </Menu.Menu>
+          )}
         </Menu>
       </Segment>
     </div>
