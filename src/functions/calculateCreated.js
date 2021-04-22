62b3a37152acb02bfc539514e28f62b879e58381
teobot <theoclapperton@outlook.com>
2021-04-22 16:54:09 +0100

    Added displayShort date function

--- src/functions/calculateCreated.js
+++ src/functions/calculateCreated.js
@@ -2,6 +2,7 @@ import { states } from "../components/Tournament/TournamentConfig";
 
 const dayjs = require("dayjs");
 const relativeTime = require("dayjs/plugin/relativeTime");
+const calendar = require("dayjs/plugin/calendar");
 
 const calculateCreated = (date) => {
   dayjs.extend(relativeTime);
@@ -11,6 +12,19 @@ const calculateCreated = (date) => {
 export const displayDate = (date) => {
   dayjs.extend(relativeTime);
   return dayjs(date).format(`MMMM DD, YYYY [at] hh:mm A`);
+};
+
+export const shortDisplayDate = (date) => {
+  dayjs.extend(calendar);
+
+  return dayjs().calendar(dayjs(date), {
+    sameDay: "[Today, ] H:mm",
+    nextDay: "MMM DD [,] H:mm",
+    nextWeek: "MMM DD [,] H:mm",
+    lastDay: "MMM DD [,] H:mm",
+    lastWeek: "MMM DD [,] H:mm",
+    sameElse: "MMM DD H:mm",
+  });
 };
 
 export const displayState = (state) => {
